ffe399f7bae139bc3fd1bddc744f6d28ea63a408
DevArrowsmith <devarrowsmith@gmail.com>
2021-08-25 15:34:59 +0100

    Update checkout to accept one of each product.

--- index.test.js
+++ index.test.js
@@ -35,4 +35,23 @@ describe("Checkout", () => {
         //Assert
         expect(result).toBe(expectedPrice);
     })
+
+    it("Scans different products and returns the total price", () => {
+        //Arrange
+        const productIdentifierA = "A";
+        const productIdentifierB = "B";
+        const productIdentifierC = "C";
+        const productIdentifierD = "D";
+        const expectedPrice = 115;
+
+        //Act
+        testCheckout.scan(productIdentifierA);
+        testCheckout.scan(productIdentifierB);
+        testCheckout.scan(productIdentifierC);
+        testCheckout.scan(productIdentifierD);
+        const result = testCheckout.checkout();
+
+        //Assert
+        expect(result).toBe(expectedPrice);
+    })
 });
--- prices.js
+++ prices.js
@@ -1,5 +1,8 @@
 const prices = {
     A: 50,
+    B: 30,
+    C: 20,
+    D: 15,
 }
 
 module.exports = prices;
